79185b5f068bd376f9f1ec1ae1d9efedcd97f26b
Bijay Timilsina <bjtimi.007@gmail.com>
2019-05-23 15:50:54 +0200

    Add top navbar component

---
+++ question_4/src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <div class="navabar-logo">
+      <img src="../assets/logo-shop.png">
+    </div>
+    <div class="navbar-title">
+      <h3>Bershka</h3>
+    </div>
+    <div class="navbar-actions">
+      <img src="../assets/icon-cart.png">
+      <span>{{0}}</span>
+    </div>
+  </nav>
+</template>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 4rem;
+  }
+  .navbar-actions {
+    position: relative;
+  }
+  img {
+    height: 5vh;
+    width: auto;
+  }
+  span {
+    position: absolute;
+    top: 40%;
+    left: 35%;
+  }
+</style>
--- question_4/src/views/Home.vue
+++ question_4/src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="home">
+    <section class="navbar">
+      <NavBar />
+    </section>
+    <section class="home-container">
+      <HomeContainer />
+    </section>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import NavBar from '@/components/NavBar.vue';
+import HomeContainer from '@/components/HomeContainer.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    NavBar,
+    HomeContainer,
   },
 };
 </script>
+<style scoped>
+  .home {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  .navbar {
+    min-height: 8vh;
+    border-bottom: 1px solid #e9e9e9;
+  }
+  .home-container {
+    padding: 0.5rem 0;
+  }
+</style>
