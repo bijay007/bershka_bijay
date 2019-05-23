bb85c25a8e0865f07d64289c303d387beecd8947
Bijay Timilsina <bjtimi.007@gmail.com>
2019-05-23 13:53:54 +0200

    Init vue app from vue cli

---
+++ question_4/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+button {
+  border: none;
+}
+</style>
