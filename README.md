a4c7bd9a5c26b495c95433358b190f127d83af1c
Bijay Timilsina <bjtimi.007@gmail.com>
2019-05-24 09:54:00 +0200

    Resolve most important question

---
+++ README.md
@@ -0,0 +1,5 @@
+# FE Test
+
+#### The answers to the questions are in the files with the respective name (eg. answer for `Question 1` is in `question_1.js`)
+
+#### Only the 4th question has its contents in a separate folder (`question_4`). Inside it there is another `README.md` where you can find instructions to run the app.
---
+++ question_most_important.js
@@ -0,0 +1,34 @@
+// Run the function below in your browser console to reveal secrets...
+const answersToEveryThing = (() => {
+  let myLifeAnswer = `The secret to having a life where you are truly happy is to just chill
+    and live life by the day. Running after new challenges everyday while sweating and swearing is not worth.
+    If there are too many challenges, then just face it with a smiley face. My favorite quote on this topic is
+    "If life gives you lemons ...
+    EAT THEM WHOLE.
+    SERIOUSLY. 
+    JUST CHOKE THEM ALL DOWN... SKIN, PULP, SEEDS, AND ALL AND DON’T BREAK EYE CONTACT. 
+    MAYBE LIFE WILL STOP BEING SUCH AN ASSHOLE IF YOU SHOW IT THAT YOU’RE DONE FUCKING AROUND.
+  `
+  let myUniverseAnswer = `By universe I suppose the world. And the world has eyes, many eyes. So many
+    eyes that you will be judged and critisized and loved and hated for every single action you do everyday
+    for your whole life. My approach to this is just chill and not give a DUCKY duck. Try to look at you action from
+    the perspective of what your younger self would think <insert your name> is correct and strive for your own progess.
+    Try to be what you wanted to be as a small ambitious kid.
+    DON'T COMPARE YOUR BEHIND THE SCENE WITH OTHERS HIGHLIGHTS
+  `
+  let myEverythingAnswer = `Strive for your own innocent moral and intellectual development and the world
+    will seem very less hostile and you will create a better small world for yourself and others.
+    SMILE EVERYDAY
+    LEARN EVERYDAY BECAUSE A MAN WILL ONLY BECOME WISE WHEN HE UNDERSTANDS THE EXTENT OF HIS IGNORANCE
+  `
+  console.log('Below are just my personal thought from my heart that I randomly typed..');
+  console.log('MY ANSWER TO LIFE...')
+  console.log(myLifeAnswer);
+  console.log('..........................................');
+  console.log('MY ANSWER TO UNIVERSE...')
+  console.log(myUniverseAnswer);
+  console.log('..........................................');
+  console.log('MY ANSWER TO EVERYTHING...')
+  console.log(myEverythingAnswer);
+  console.log('..........................................');
+})()
